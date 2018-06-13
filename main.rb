cb4642f6e38f08dd2a0f35e9eed5f0d1f18e77c7
Chris Dunphy <chris.dunphy@fglsports.com>
2018-06-13 16:50:18 -0600

    Ran rufo on Ruby code

--- main.rb
+++ main.rb
@@ -1,16 +1,16 @@
-require_relative 'company'
+require_relative "company"
 
 include RcdCompany
 
-puts 'Define some people'
+puts "Define some people"
 
-rcd = Employee.new('Chris Dunphy', 43, :male, 'FGL Sports Inc.', 32139, 999999.00)
-lad = Person.new('Lori-Ann Dunphy', 47, :female)
+rcd = Employee.new("Chris Dunphy", 43, :male, "FGL Sports Inc.", 32139, 999999.00)
+lad = Person.new("Lori-Ann Dunphy", 47, :female)
 
 people = [rcd, lad]
 
 people.each do |person|
-    puts person
+  puts person
 end
 
 puts 'Let\'s give all employees a raise...'
@@ -19,10 +19,10 @@ puts
 company = [rcd]
 
 company = company.map do |employee|
-    employee.salary *= 2
-    employee
+  employee.salary *= 2
+  employee
 end
 
 company.each do |employee|
-    puts employee
+  puts employee
 end
